dd3a3e1804576c7b866bc25546db10add9183ea9
Alexander Vasin <alvassin@yandex-team.ru>
2023-03-08 14:51:23 +0300

    Fix annotations

--- aiohttp_s3_client/client.py
+++ aiohttp_s3_client/client.py
@@ -60,7 +60,7 @@ class AwsDownloadError(AwsError):
 @threaded
 def concat_files(
     target_file: Path, files: t.List[io.BytesIO], buffer_size: int,
-):
+) -> None:
     with target_file.open("ab") as fp:
         for file in files:
             file.seek(0)
@@ -75,7 +75,7 @@ def concat_files(
 @threaded
 def write_from_start(
     file: io.BytesIO, chunk: bytes, range_start: int, pos: int,
-):
+) -> None:
     file.seek(pos - range_start)
     file.write(chunk)
 
@@ -83,7 +83,7 @@ def write_from_start(
 @threaded
 def pwrite_absolute_pos(
     fd: int, chunk: bytes, range_start: int, pos: int,
-):
+) -> None:
     os.pwrite(fd, chunk, pos)
 
 
@@ -150,7 +150,7 @@ class S3Client:
         )
 
     @property
-    def url(self):
+    def url(self) -> URL:
         return self._url
 
     def request(
@@ -241,7 +241,7 @@ class S3Client:
         self, object_name: str,
         data: t.Union[None, bytes, str, t.AsyncIterable[bytes]] = None,
         **kwargs,
-    ):
+    ) -> RequestContextManager:
         return self.request("POST", object_name, data=data, **kwargs)
 
     def put_file(
@@ -379,7 +379,7 @@ class S3Client:
         part_upload_tries: int = 3,
         calculate_content_sha256: bool = True,
         **kwargs,
-    ):
+    ) -> None:
         """
         Upload data from a file with multipart upload
 
@@ -613,7 +613,7 @@ class S3Client:
         range_get_tries: int = 3,
         buffer_size: int = PAGESIZE * 32,
         **kwargs,
-    ):
+    ) -> None:
         """
         Download object in parallel with requests with Range.
         If file will change while download is in progress -
